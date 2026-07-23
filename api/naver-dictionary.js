7ee022cda9266b21bebc3c69b68c5951b4ad7a9f
hikim1162-sketch <hikim1162@gmail.com>
2026-07-23 17:49:55 +0900

    20260723-05
    재배포

--- api/naver-dictionary.js
+++
@@ -1,4 +0,0 @@
-import handler from "../netlify-private-app/netlify/functions/naver-dictionary.js";
-import { adapt } from "../netlify-private-app/api/_adapter.js";
-
-export default adapt(handler);
---
+++ netlify-private-app/api/naver-dictionary/index.js
@@ -0,0 +1,4 @@
+import handler from "../../netlify/functions/naver-dictionary.js";
+import { adapt } from "../_adapter.js";
+
+export default adapt(handler);
